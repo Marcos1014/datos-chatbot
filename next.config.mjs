6a2611028670ba4de289622435e6e4a3bf2001d6
Marcos1014 <marcos.quiroga1023@gmail.com>
2024-10-30 19:24:23 -0300

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 // next.config.mjs
 export default {
-    output: 'export', 
-    images: {
-      unoptimized: true,
-    },
-    basePath: '/datos-chatbot', // Cambia 'datos-chatbot' por el nombre de tu repositorio
-    assetPrefix: '/datos-chatbot/', // Mismo nombre del repositorio
-  };
-  
+  reactStrictMode: true,
+};
